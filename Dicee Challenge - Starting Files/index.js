6e23ffce5f9e956b3a1b7ac9903db8c6e24d7f55
AkritiSubedi <aakusubedi075@gmail.com>
2023-12-27 20:30:50 +0545

    dicee

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -16,9 +16,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML="🚩play 1 Wins!";
 }
+//if players 2 wins
 else if(randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML="play 2 Wins!🚩";
 }
+// in case of draw
 else{
     document.querySelector("h1").innerHTML="Draw !";
 }
